b583450d9371240b04540bbd5726eaca3b6c0dff
Alex Alvarez Gárciga <alexalvarezgarciga@MacBookAir.attlocal.net>
2024-12-02 20:55:10 -0500

    turning off powered by the header as I see no reason to have it enabled .

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,22 @@
 import type { NextConfig } from "next";
+import { PHASE_DEVELOPMENT_SERVER } from "next/constants.js";
 
-/** @type {import('next').NextConfig} */
-const nextConfig: NextConfig = {
-  /* config options here */
-  experimental: {
-    // experimental typescript "statically typed links"
-    // https://nextjs.org/docs/app/api-reference/next-config-js/typedRoutes
-    // currently false in prod until Issue #62335 is fixed
-    // https://github.com/vercel/next.js/issues/62335
-    typedRoutes: true,    
-  },
+const nextConfig: NextConfig = (phase: string) => {
+  /** @type {import('next').NextConfig} */
+  const nextConfigOptions = {
+    /* config options here */
+    reactStrictMode: true,
+    poweredByHeader: false,
+    experimental: {
+      // experimental typescript "statically typed links"
+      // https://nextjs.org/docs/app/api-reference/next-config-js/typedRoutes
+      // currently false in prod until PR #67824 lands in a stable release
+      // https://github.com/vercel/next.js/pull/67824
+      typedRoutes: phase === PHASE_DEVELOPMENT_SERVER ? true : false,
+    },
+  }
+
+  return nextConfigOptions
 };
 
 export default nextConfig;
