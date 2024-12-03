44461cf7d017c00d778e7b7693ef6285206d0989
Alex Alvarez Gárciga <alexalvarezgarciga@MacBookAir.attlocal.net>
2024-12-02 20:33:24 -0500

    Typescript support is done correctly.

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
+/** @type {import('next').NextConfig} */
 const nextConfig: NextConfig = {
   /* config options here */
+  experimental: {
+    // experimental typescript "statically typed links"
+    // https://nextjs.org/docs/app/api-reference/next-config-js/typedRoutes
+    // currently false in prod until Issue #62335 is fixed
+    // https://github.com/vercel/next.js/issues/62335
+    typedRoutes: true,    
+  },
 };
 
 export default nextConfig;
